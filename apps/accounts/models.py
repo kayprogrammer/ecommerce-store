354db25c5bd6bf92d2e97b87c69180b213e25706
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2024-05-01 13:12:03 +0100

    chore: setup

---
+++ apps/accounts/models.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from apps.common.models import BaseModel, image_folder_to_upload
+from .managers import CustomUserManager
+
+
+class User(AbstractBaseUser, BaseModel, PermissionsMixin):
+    first_name = models.CharField(
+        verbose_name=(_("First name")), max_length=25, null=True
+    )
+    last_name = models.CharField(
+        verbose_name=(_("Last name")), max_length=25, null=True
+    )
+    email = models.EmailField(verbose_name=(_("Email address")), unique=True)
+    avatar = models.ImageField(upload_to=image_folder_to_upload("avatars"), null=True)
+
+    terms_agreement = models.BooleanField(default=False)
+    is_email_verified = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    @property
+    def avatar_url(self):
+        try:
+            url = self.avatar.url
+        except:
+            url = "https://res.cloudinary.com/kay-development/image/upload/v1679787683/important/brad_dozo7x.jpg"
+        return url
+
+    def __str__(self):
+        return self.full_name
---
+++ apps/common/models.py
@@ -0,0 +1,20 @@
+import uuid
+from django.db import models
+
+
+class BaseModel(models.Model):
+    id = models.UUIDField(
+        default=uuid.uuid4, primary_key=True, editable=False, unique=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+def image_folder_to_upload(subfolder=""):
+    folder = f"ecommerce-store/{subfolder}/"
+    if subfolder == "":
+        folder = "ecommerce-store/"
+    return folder
