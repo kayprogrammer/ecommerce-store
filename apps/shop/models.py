0436a3b8712e2fa0ad1013c4851a2174b92595ef
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2024-05-08 11:44:26 +0100

    chore: shop models

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from autoslug import AutoSlugField
+from apps.common.models import BaseModel
 
-# Create your models here.
+class Size(BaseModel):
+    value = models.CharField(max_length=5)
+
+class Colour(BaseModel):
+    value = models.CharField(max_length=20)
+
+class Category(BaseModel):
+    name = models.CharField(max_length=20)
+    slug = AutoSlugField(populate_from=name, always_update=True)
+
+class Product(BaseModel):
+    name = models.CharField(max_length=100)
+    slug = AutoSlugField(populate_from=name, always_update=True)
+    desc = models.TextField()
+    price_old = models.DecimalField(max_digits=10, decimal_places=True)
+    price_current = models.DecimalField(max_digits=10, decimal_places=True)
+    sizes = models.ManyToManyField(Size)
+    colours = models.ManyToManyField(Colour)
+
+class ProductImage():
+    product = models.ForeignKey(Product, on_delete=models.C)
