3cdb1305b50bfe2f0f0bffd36de276b0511305a2
kene <kenechiifeanyi@gmail.com>
2024-07-27 13:09:51 +0100

    tests: accounts

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from django.conf import settings
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
-# Create your tests here.
+from apps.common.utils import TestUtil
+
+settings.TESTING = True
+
+token_generator = PasswordResetTokenGenerator()
+
+
+class TestAccounts(TestCase):
+    login_url = "/accounts/login/"
+    logout_url = "/accounts/logout/"
+
+    def setUp(self):
+        self.new_user = TestUtil.new_user()
+
+    def test_login(self):
+        # GET #
+        response = self.client.get(self.login_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "accounts/login.html")
+
+    def test_logout(self):
+        verified_user = self.new_user
+
+        # Ensures A user logs out successfully
+        self.client.login(email=verified_user.email, password="testpassword")
+        response = self.client.get(self.logout_url)
+        self.assertRedirects(
+            response,
+            "/",
+            status_code=302,
+            target_status_code=200,
+            fetch_redirect_response=True,
+        )
+
+    # No be me go do everything. Write tests for google and facebook auth yourself. I don tire.
--- apps/common/utils.py
+++ apps/common/utils.py
@@ -1,7 +1,62 @@
 from django.db.models import Avg, Value, FloatField, Count
 from django.db.models.functions import Coalesce
+from django.utils import timezone
+from apps.accounts.models import User
+from apps.shop.models import Category, Colour, Country, Product, Size
 
 REVIEWS_AND_RATING_ANNOTATION = {
     "reviews_count": Count("reviews"),
     "avg_rating": Coalesce(Avg("reviews__rating"), Value(0), output_field=FloatField()),
 }
+
+class TestUtil:
+    def new_user():
+        user_dict = {
+            "first_name": "Test",
+            "last_name": "Name",
+            "email": "test@example.com",
+            "password": "testpassword",
+        }
+        user = User.objects.create_user(**user_dict)
+        return user
+
+    def another_user():
+        create_user_dict = {
+            "first_name": "AnotherTest",
+            "last_name": "User",
+            "email": "anothertestuser@example.com",
+            "password": "anothertestuser123",
+        }
+        user = User.objects.create_user(**create_user_dict)
+        return user
+
+    def create_category():
+        category, _ = Category.objects.get_or_create(name="TestCategory")
+        return category
+    
+    def create_size_and_color():
+        size, _ = Size.objects.get_or_create(value="M")
+        color, _ = Colour.objects.get_or_create(value="Black")
+        return size, color
+    
+    def create_product(user):
+        # Create Category
+        category = TestUtil.create_category()
+        size, color = TestUtil.create_size_and_color()
+        # Create Product
+        product_dict = {
+            "name": "New Test Listing",
+            "desc": "New description",
+            "category": category,
+            "price_old": 1000.00,
+            "price_current": 500.00,
+            "in_stock": 100,
+        }
+        product, _ = Product.objects.get_or_create(name=product_dict["name"], defaults=product_dict)
+        product.sizes.add(size)
+        product.colours.add(color)
+        return product
+    
+    def create_country(self):
+        country, _ = Country.objects.get_or_create(name="TestCountry", code="TC")
+        return country
